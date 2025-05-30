c8603ac96860105029ad938d54e5941a36eb1af6
anz-cell <mathematicsgeek1@outlook.com>
2025-05-30 22:55:56 +0400

    feat: enhance product listing with infinite scroll, improved product card component, and new product view layout

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,26 +1,32 @@
+import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
+import { ProductView } from "@/modules/products/ui/views/product-list-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 
 interface Props {
     params: Promise<{
         subcategory: string;
-    }>
+    }>,
+    searchParams: Promise<SearchParams>;
 }
-const Page = async ({ params }: Props) => {
-const { subcategory } = await params;
+const Page = async ({ params, searchParams }: Props) => {
+    const { subcategory } = await params;
+    const filters = await loadProductFilters(searchParams);
+
+    console.log(JSON.stringify(filters), "THIS IS FROM RSC");
 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
         category: subcategory,
+        ...filters,
     }));
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<ProductListSkeleton />}>
-            <ProductList category={subcategory}/>
-            </Suspense>
+          <ProductView category={subcategory}/>
         </HydrationBoundary>
     );
 };
