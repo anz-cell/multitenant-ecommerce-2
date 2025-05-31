6e8bbb617854af36c0d382bbcc1ab4f9796dac69
anz-cell <mathematicsgeek1@outlook.com>
2025-05-31 15:41:43 +0400

    feat: add product view and star rating components, enhance currency formatting, and implement tenant URL generation

---
+++ src/modules/products/ui/views/product-view.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { StarRating } from "@/components/star-rating";
+import { formatCurrency, generateTenantURL } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductViewProps {
+  productId: string;
+  tenantSlug: string;
+}
+
+export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.products.getOne.queryOptions({ id: productId })
+  );
+
+  return (
+    <div className="px-4 lg:px-12 py-10">
+      <div className="border rounded-sm bg-white overflow-hidden">
+        <div className="relative aspect-[3.9] border-b">
+          <Image
+            src={data.image?.url || "/next.svg"}
+            alt={data.name}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-6">
+          <div className="col-span-4">
+            <div className="p-6">
+              <h1 className="text-4xl font-medium">{data.name}</h1>
+            </div>
+            <div className="border-y flex">
+              <div className="px-6 py-4 flex items-center justify-center border-r">
+                <div className="px-2 py-1 border bg-pink-400 w-fit">
+                  <p className="text-base font-medium">
+                    {formatCurrency(data.price)}
+                  </p>
+                </div>
+              </div>
+              <div className="px-6 py-4 flex items-center justify-center lg:border-r">
+                <Link
+                  href={generateTenantURL(tenantSlug)}
+                  className="flex items-center gap-2"
+                >
+                  {data.tenant.image?.url && (
+                    <Image
+                      src={data.tenant.image.url}
+                      alt={data.tenant.name}
+                      width={20}
+                      height={20}
+                      className="rounded-full border shrink-0 size-[20px]"
+                    />
+                  )}
+                  <p className="text-base underline font-medium">
+                    {data.tenant.name}
+                  </p>
+                </Link>
+              </div>
+              <div className="hidden lg:flex px-6 py-4 items-center justify-center">
+                <div className="flex items-center gap-1">
+                  <StarRating rating={4} iconClassName="size-4" />
+                </div>
+              </div>
+            </div>
+            <div className="block lg:hidden px-6 py-4 items-center justify-center border-b">
+              <div className="flex items-center gap-1">
+                <StarRating rating={4} iconClassName="size-4" />
+                <p className="text-base font-medium">{5} ratings</p>
+              </div>
+            </div>
+            <div className="p-6">
+              {data.description ? (
+                <p>{data.description}</p>
+              ) : (
+                <p className="font-medium text-muted-foreground italic">
+                  No description provided
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
