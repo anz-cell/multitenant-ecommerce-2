57112ed59d1824e34ef6a5a9ad04a2f95a17023e
anz-cell <mathematicsgeek1@outlook.com>
2025-06-01 18:34:12 +0400

    feat: integrate Stripe for checkout process, add order management, and implement webhook handling

--- src/modules/checkout/ui/components/checkout.sidebar.tsx
+++ src/modules/checkout/ui/components/checkout.sidebar.tsx
@@ -4,16 +4,16 @@ import { CircleXIcon } from "lucide-react";
 
 interface CheckoutSidebarProps {
   total: number;
-  onCheckout: () => void;
+  onPurchase: () => void;
   isCanceled?: boolean;
-  isPending?: boolean;
+  disabled?: boolean;
 }
 
 export const CheckoutSidebar = ({
   total,
-  onCheckout,
+  onPurchase,
   isCanceled,
-  isPending,
+  disabled,
 }: CheckoutSidebarProps) => {
   return (
     <div className="border rounded-md overflow-hidden bg-white flex flex-col">
@@ -24,8 +24,8 @@ export const CheckoutSidebar = ({
       <div className="p-4 flex items-center justify-center">
         <Button
           variant="elevated"
-          disabled={isPending}
-          onClick={onCheckout}
+          disabled={disabled}
+          onClick={onPurchase}
           size="lg"
           className="text-base w-full text-white bg-primary hover:bg-pink-400 hover:text-black"
         >
@@ -36,8 +36,8 @@ export const CheckoutSidebar = ({
         <div className="p-4 flex justify-center items-center border-t">
           <div className="bg-red-100 border border-red-400 font-medium px-4 py-3 rounded flex items-center w-full">
             <div className="flex items-center">
-                          <CircleXIcon className="size-6 mr-2 fill-red-500 text-red-100" />
-                          <span>Checkout failed. Please try again.</span>
+              <CircleXIcon className="size-6 mr-2 fill-red-500 text-red-100" />
+              <span>Checkout failed. Please try again.</span>
             </div>
           </div>
         </div>
--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useCart } from "../../hooks/use-cart";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -9,13 +9,17 @@ import { generateTenantURL } from "@/lib/utils";
 import { CheckoutItem } from "../components/checkout-item";
 import { CheckoutSidebar } from "../components/checkout.sidebar";
 import { InboxIcon, LoaderIcon } from "lucide-react";
+import { useCheckoutStates } from "../../hooks/use-checkout-states";
+import { useRouter } from "next/navigation";
 
 interface CheckoutViewProps {
   tenantSlug: string;
 }
 
 export const CheckoutViiew = ({ tenantSlug }: CheckoutViewProps) => {
-  const { productIds, removeProduct, clearAllCarts } = useCart(tenantSlug);
+  const router = useRouter();
+  const [states, setStates] = useCheckoutStates();
+  const { productIds, removeProduct, clearCart } = useCart(tenantSlug);
   const trpc = useTRPC();
   const { data, error, isLoading } = useQuery(
     trpc.checkout.getProducts.queryOptions({
@@ -23,32 +27,58 @@ export const CheckoutViiew = ({ tenantSlug }: CheckoutViewProps) => {
     })
   );
 
+  const purchase = useMutation(
+    trpc.checkout.purchase.mutationOptions({
+      onMutate: () => {
+        setStates({ success: false, cancel: false });
+      },
+      onSuccess: (data) => {
+        window.location.href = data.url;
+      },
+      onError: (error) => {
+        if (error.data?.code === "UNAUTHORIZED") {
+          router.push("/sign-in");
+        }
+
+        toast.error(error.message);
+      },
+    })
+  );
+
+  useEffect(() => {
+    if (states.success) {
+      setStates({ success: false, cancel: false });
+      clearCart();
+      router.push("/products");
+    }
+  }, [states.success, clearCart, router, setStates]);
+
   useEffect(() => {
     if (error?.data?.code === "NOT_FOUND") {
-      clearAllCarts();
+      clearCart();
       toast.warning("Invalid products found, cart cleared");
     }
-  }, [error, clearAllCarts]);
-    
-    if (isLoading) {
-        return (
-          <div className="lg:pt-16 pt-4 px-4 lg:px-12">
-            <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
-              <LoaderIcon className="text-muted-foreground animate-spin" />
-            </div>
-          </div>
-        );
-    }
+  }, [error, clearCart]);
+
+  if (isLoading) {
+    return (
+      <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+        <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
+          <LoaderIcon className="text-muted-foreground animate-spin" />
+        </div>
+      </div>
+    );
+  }
 
   if (data?.totalDocs === 0) {
-      return (
-        <div className="lg:pt-16 pt-4 px-4 lg:px-12">
-          <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
-            <InboxIcon />
-            <p className="text-base font-medium">No products found</p>
-          </div>
+    return (
+      <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+        <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
+          <InboxIcon />
+          <p className="text-base font-medium">No products found</p>
         </div>
-      );
+      </div>
+    );
   }
 
   return (
@@ -75,9 +105,9 @@ export const CheckoutViiew = ({ tenantSlug }: CheckoutViewProps) => {
         <div className="lg:col-span-3">
           <CheckoutSidebar
             total={data?.totalPrice || 0}
-            onCheckout={() => {}}
-            isCanceled={false}
-            isPending={false}
+            onPurchase={() => purchase.mutate({ tenantSlug, productIds })}
+            isCanceled={states.cancel}
+            disabled={purchase.isPending}
           />
         </div>
       </div>
